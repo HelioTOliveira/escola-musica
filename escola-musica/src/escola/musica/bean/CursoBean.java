4b7a9dce694867538c53f85df95781f2fa917ef5
helio <helio@Notebook>
2017-07-11 20:49:51 -0300

    Feita aula 3,4 e 5.
    Validando campos e criado tabela para listar curso

--- escola-musica/src/escola/musica/bean/CursoBean.java
+++ escola-musica/src/escola/musica/bean/CursoBean.java
@@ -1,13 +1,56 @@
 package escola.musica.bean;
 
-import javax.annotation.ManagedBean;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 import escola.musica.modelo.Curso;
+import escola.musica.modelo.TipoCurso;
 
 @ManagedBean
+@SessionScoped
 public class CursoBean {
 
 	private Curso curso = new Curso();
+	private List<TipoCurso> tipos = Arrays.asList(TipoCurso.values());
+	private List<Curso> cursos = new ArrayList<Curso>();
+
+	public String salvar(){
+		cursos.add(curso);
+		curso = new Curso();
+		
+		FacesContext.getCurrentInstance().addMessage(null, 
+				new FacesMessage("Curso Salvo com Sucesso"));
+		return "curso_lista?faces-redirect=true";
+	}
+	
+	public String getDataAtual(){
+		
+		return new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+	}
+	
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+
+	public List<TipoCurso> getTipos() {
+		return tipos;
+	}
+
+	public void setTipos(List<TipoCurso> tipos) {
+		this.tipos = tipos;
+	}
 
 	public Curso getCurso() {
 		return curso;
