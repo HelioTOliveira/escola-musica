cab2180a2d359ec4258d714b9be4828c2edf2e08
helio <helio@Notebook>
2017-07-20 20:53:27 -0300

    Aula 28 concluido o cadastro de aluno porem esta com erro ao inserir BD

--- escola-musica/src/escola/musica/bean/AlunoBean.java
+++ escola-musica/src/escola/musica/bean/AlunoBean.java
@@ -29,6 +29,7 @@ public class AlunoBean implements Serializable {
 	private Aluno aluno;
 	private List<Aluno> alunos;
 	private List<Estado> estados;
+	private Integer idCidade;
 	
 	public void iniciarBean(){
 		alunos = new GenericDAO<Aluno>(Aluno.class).listarTodos();
@@ -40,6 +41,7 @@ public class AlunoBean implements Serializable {
 	}
 	
 	public void salvar(){
+		aluno.getEndereco().setCidade(new GenericDAO<Cidade>(Cidade.class).obterPorId(idCidade));
 		new GenericDAO<Aluno>(Aluno.class).salvar(aluno);
 		
 		FacesContext.getCurrentInstance().addMessage(null, 
@@ -98,5 +100,14 @@ public class AlunoBean implements Serializable {
 	public void setEstados(List<Estado> estados) {
 		this.estados = estados;
 	}
+
+	public Integer getIdCidade() {
+		return idCidade;
+	}
+
+	public void setIdCidade(Integer idCidade) {
+		this.idCidade = idCidade;
+	}
+	
 	
 }
