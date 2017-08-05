091a19cde9fb96dde3181e35d38e886362e56771
helio <helio@Notebook>
2017-08-05 19:34:50 -0300

    Aula 55 criado Drag Drop para incluir alunos matrículados no curso.

---
+++ escola-musica/src/escola/musica/bean/TurmaBean.java
@@ -0,0 +1,39 @@
+package escola.musica.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import escola.musica.modelo.Matricula;
+import escola.musica.servico.MatriculaServico;
+
+@Controller("turmaBean")
+@Scope("session")
+public class TurmaBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 374615860770008608L;
+
+	private List<Matricula> matriculas;
+
+	@Autowired
+	private MatriculaServico matriculaServico;
+
+	public void iniciarBean() {
+		matriculas = matriculaServico.listarTodasAtivas();
+	}
+
+	public List<Matricula> getMatriculas() {
+		return matriculas;
+	}
+
+	public void setMatriculas(List<Matricula> matriculas) {
+		this.matriculas = matriculas;
+	}
+
+}
