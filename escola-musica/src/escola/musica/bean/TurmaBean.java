a6f8b04744aad1ec146a33db4c6f628fde3b2fc6
helio <helio@Notebook>
2017-08-24 18:54:22 -0300

    Aula 77 Criando tela de cadastro para turmas parte 3

---
+++ escola-musica/src/escola/musica/bean/TurmaBean.java
@@ -0,0 +1,109 @@
+package escola.musica.bean;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+import escola.musica.modelo.Curso;
+import escola.musica.modelo.Matricula;
+import escola.musica.modelo.Semestre;
+import escola.musica.modelo.Turma;
+import escola.musica.servico.CursoServico;
+import escola.musica.servico.MatriculaServico;
+import escola.musica.servico.TurmaServico;
+import escola.musica.util.Mensagem;
+
+@Controller("turmaBean")
+public class TurmaBean implements Serializable {
+
+	private static final long serialVersionUID = 374615860770008608L;
+
+	private Turma turma;
+	private List<Turma> turmas;
+	private List<Semestre> semestres;
+	private List<Curso> cursos;
+	private List<Matricula> matriculas;
+
+	@Autowired
+	private TurmaServico turmaServico;
+	@Autowired
+	private CursoServico cursoServico;
+	@Autowired
+	private MatriculaServico matriculaServico;
+	
+	public void iniciarBean(){
+		semestres =  Arrays.asList(Semestre.values());
+		atualizarTurmas();
+		matriculas =  matriculaServico.listarTodasAtivas();
+		cursos = cursoServico.listarTodos();
+	}
+	
+	public void novarTurma(){
+		turma = new Turma();
+	}
+	
+	public void salvarTurma(){
+		turmaServico.salvar(turma);
+		turma = null;
+		atualizarTurmas();
+		Mensagem.mensagemInformacao("Turma salva com sucesso!");
+	}
+	
+	public void editarTurma(Turma turma){
+		this.turma = turma;
+	}
+	
+	public void cancelarTurma(){
+		this.turma = null;
+	}
+	
+	private void atualizarTurmas(){
+		turmas = turmaServico.listarTodas();
+	}
+	
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	public List<Turma> getTurmas() {
+		return turmas;
+	}
+
+	public void setTurmas(List<Turma> turmas) {
+		this.turmas = turmas;
+	}
+
+	public List<Semestre> getSemestres() {
+		return semestres;
+	}
+
+	public void setSemestres(List<Semestre> semestres) {
+		this.semestres = semestres;
+	}
+
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+
+	public List<Matricula> getMatriculas() {
+		return matriculas;
+	}
+
+	public void setMatriculas(List<Matricula> matriculas) {
+		this.matriculas = matriculas;
+	}
+
+}
